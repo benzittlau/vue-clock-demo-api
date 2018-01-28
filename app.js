20526c4983716182b9470c70de4f258156f979bf
Ben Zittlau <ben@zittlau.ca>
2018-01-28 12:01:21 -0500

    Implement a very basic timezone API server.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const moment = require('moment-timezone')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/timezone[\/]:zone', (req, res) =>
+  res.send(moment().tz(req.params.zone).format('h:mma z'))
+)
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
